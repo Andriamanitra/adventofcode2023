53bb3bb682b7326ad5e1b8c32c8bd3790a048dc1
Andriamanitra <mikko77792@gmail.com>
2023-12-05 10:16:20 +0200

    solve day 5 part 2 in Ruby

--- day05/sol.rb
+++ day05/sol.rb
@@ -1,7 +1,9 @@
-# Part 1
 s = File.read("input.txt")
-seeds = s.lines[0].split[1..].map(&:to_i)
 sections = s.split("\n\n")[1..]
+
+# Part 1
+seeds = s.lines[0].split[1..].map(&:to_i)
+
 sections.map do |sect|
   xs = sect.lines[1..].map{_1.split.map(&:to_i)}
   seeds.map! do |seed|
@@ -11,4 +13,47 @@ sections.map do |sect|
     end
   end
 end
-p seeds.min
+
+puts "Part 1:", seeds.min
+
+
+# Part 2
+seeds = s.lines[0].split[1..].map(&:to_i).each_slice(2).map{[_1, _1+_2-1]}
+
+sections.map do |sect|
+  xs = sect.lines[1..].map{_1.split.map(&:to_i)}.sort_by{_2}
+  new_seeds = []
+
+  xs.each do |dest,src,len|
+    next_seeds = []
+    a = src
+    b = src + len - 1
+    seeds.each do |c, d|
+      # A-C-D-B
+      if a <= c && d <= b # full overlap
+        new_seeds << [c - src + dest, d - src + dest]
+      # C-A-B-D
+      elsif c < a && b < d # inside
+        next_seeds << [c, a-1]
+        new_seeds << [a - src + dest, b - src + dest]
+        next_seeds << [b+1, d]
+      # A-C-B-D
+      elsif a < c && c < b && b < d # partial overlap
+        new_seeds << [c - src + dest, b - src + dest]
+        next_seeds << [b+1, d]
+      # C-A-D-B 
+      elsif c < a && a < d && d < b # partial overlap
+        next_seeds << [c, a-1]
+        new_seeds << [a - src + dest, d - src + dest]
+      # A-B-C-D or C-D-A-B
+      else # no overlap
+        next_seeds << [c, d]
+      end
+    end
+    seeds = next_seeds
+  end
+
+  seeds += new_seeds
+end
+
+puts "Part 2:", seeds.map{|a,b| a }.min
